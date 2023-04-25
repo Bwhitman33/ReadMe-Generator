16f39143724fdb15d1897d318b5587fe284f1336
Bwhitman33 <bwhitman33@gmail.com>
2023-04-24 22:31:58 -0400

    Fixed issues with markdown file causing the readme's table of contents to not link to their sections

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,53 +1,54 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// function renderLicenseBadge(license) {}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// function renderLicenseLink(license) {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
   <br>
-  ## Description
+  # Description
   ${data.description}
   <br>
-  ## Table of Contents
-  1. [Installation](#installation)
-  2. [Usage](#usage)
-  3. [Contributing](#contributing)
-  4. [Tests](#tests)
-  5. [Languages Used](#languages)
-  6. [Questions](#questions)
-  <br>
   
-  ## Installation
+  # Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Languages Used](#languages)
+  - [Questions](#questions)
+  <br>
+
+  # Installation
   See below for any of this project's dependencies - 
   ${data.installation}
   <br>
 
-  ## Usage
+  # Usage
   ${data.usage}
   <br>
 
-  ## Contributing
+  # Contributing
   ${data.contributing}
   <br>
 
-  ## Tests
+  # Tests
   ${data.tests}
   <br>
 
-  ## Languages Used
+  # Languages Used
   ${data.languages}
   <br>
 
-  ## Questions
+  # Questions
   Any questions can be asked at ${data.userEmail}. 
   <br> 
   The github repository for this project can be found at [${data.gitUser}](${data.gitRepo})
